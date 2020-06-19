ce7b6811397b5762d985b3bb49a2e3b581b2b88c
Adriana Jimenez <adriana.f.jimenez@gmail.com>
2020-06-19 17:14:05 -0400

    updated content

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
 const qwerty = document.getElementById('qwerty');
-const phrase = document.getElementById('phrase');
+const phraseUL = document.querySelector('#phrase ul');
 const startButton = document.querySelector('.btn__reset');
 const overlay = document.getElementById('overlay');
+const tries = document.querySelector('#scoreboard ol');
+const title = document.querySelector('.title');
 let missed = 0;
-
 
 let phrases = ["my precious",
     "just keep swimming",
     "why so serious",
     "nobody puts baby in the corner",
     "to infinity and beyond",
-    "may the forth be with you"
+    "may the force be with you"
 ]
 
 // listen for the start game button to be pressed 
 startButton.addEventListener('click', () => {
     overlay.style.display = 'none';
+    resetGame();
 });
 
 // return a random phrase from an array
@@ -24,13 +26,12 @@ function getRandomPhrasesAsArray(arr) {
     let word = randomPhrase.split("");
     return word;
 };
-const phraseArray = getRandomPhrasesAsArray(phrases)
 
 // adds the letters of a string to the display
 // loop through phrase array
 // create li elements and add class(name)
 function addPhraseToDisplay(arr) {
-    const ul = document.querySelector('#phrase ul');
+    //const ul = document.querySelector('#phrase ul');
     for (let i = 0; i < arr.length; i++) {
         const li = document.createElement('li');
         li.textContent = arr[i];
@@ -39,43 +40,91 @@ function addPhraseToDisplay(arr) {
         } else {
             li.className = 'letter';
         }
-        ul.appendChild(li);
+        phraseUL.appendChild(li);
     }
 }
-addPhraseToDisplay(phraseArray);
 
-// // check if a letter is in the phrase 
+// check if a letter is in the phrase 
 function checkLetter(button) {
     const lis = document.querySelectorAll('.letter');
-    let match = null
+    let match = null;
     for (let i = 0; i < lis.length; i++) {
         if (button.textContent.toUpperCase() == lis[i].textContent.toUpperCase()) {
-            lis[i].className += 'show';
+            lis[i].classList.add('show');
             match = button.textContent;
         }
     }
-    return console.log(match);
+    return match;
 }
 
-
-// // listen for the onscreen keyboard to be clicked 
+// listen for the onscreen keyboard to be clicked 
 qwerty.addEventListener('click', (event) => {
-    const button = event.target;
-    const letterFound = checkLetter(button);
+    if (event.target.tagName == 'BUTTON') {
+        const button = event.target;
+        const letterFound = checkLetter(button);
+        let heart = tries.firstElementChild;
 
-    if (event.target.tagName == 'button') {
-        button.className += 'chosen';
-        button.disabled = 'true';
-
-        if (event.target.className !== 'chosen') {
-            letterFound == null;
-            const tries = document.getElementById('scoreboard');
-            tries.removeChild(tries.firstChild);
-            missed ++;
+        if (letterFound == null) {
+            console.log("letter not found");
+            button.classList.add('wrongLetter');
+            tries.removeChild(heart);
+            missed += 1;
+        } else {
+            console.log("letter found")
+            button.classList.add('chosen');
+            // css transition??
         }
+        button.disabled = true;
     }
+    checkWin();
 });
 
 // check if the game has been won or lost 
-// const checkWin = () => {
-// }
+function checkWin() {
+    let letters = document.getElementsByClassName('letter');
+    let shown = document.getElementsByClassName('show');
+
+    if (letters.length == shown.length) {
+        overlay.className = 'win';
+        const win = document.querySelector('.win');
+        win.style.display = 'flex';
+        title.textContent = 'You won!';
+        startButton.textContent = 'Play Again';
+    } else {
+        if (missed > 4) {
+            overlay.className = 'lose';
+            const lose = document.querySelector('.lose');
+            lose.style.display = 'flex';
+            title.textContent = 'You lost!';
+            startButton.textContent = 'Try Again';
+
+        }
+    }
+}
+
+function resetGame() {
+    // make sure the Phrase UL is empty
+    phraseUL.innerHTML = '';
+
+    // randomly pick a phrase
+    const phraseArray = getRandomPhrasesAsArray(phrases);
+    console.log(phraseArray);
+
+    // display phrase on webpage
+    addPhraseToDisplay(phraseArray);
+
+    // recreate the keyboard
+    removeButton = document.querySelectorAll('button');
+    for (let i = 0; i < removeButton.length; i++) {
+        removeButton[i].disabled = false;
+        removeButton[i].className = '';
+    }
+
+    //bring back hearts
+    const heartsRow = document.querySelectorAll('#scoreboard ol li');
+    for (let i = 0; i < heartsRow.length; i++) {
+        let imgHeart = heartsRow[i].getElementsByTagName('img');
+        imgHeart.src = "images/liveHeart.png";
+    }
+    missed = 0;
+}
